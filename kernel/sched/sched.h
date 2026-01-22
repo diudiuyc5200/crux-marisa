ef755bb93763eef807b7d3e7fca69735bbfdbc56
diudiuyc5200 <17766728994@163.com>
2026-01-22 15:20:31 +0800

    Update sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2490,14 +2490,6 @@ extern int update_preferred_cluster(struct related_thread_group *grp,
 extern void set_preferred_cluster(struct related_thread_group *grp);
 extern void add_new_task_to_grp(struct task_struct *new);
 
-#define NO_BOOST 0
-#define FULL_THROTTLE_BOOST 1
-#define CONSERVATIVE_BOOST 2
-#define RESTRAINED_BOOST 3
-#define FULL_THROTTLE_BOOST_DISABLE -1
-#define CONSERVATIVE_BOOST_DISABLE -2
-#define RESTRAINED_BOOST_DISABLE -3
-#define MAX_NUM_BOOST_TYPE (RESTRAINED_BOOST+1)
 
 static inline int asym_cap_siblings(int cpu1, int cpu2)
 {
