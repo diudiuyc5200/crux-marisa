7c81fba7bdae62a34a11199fa781322a74437c27
diudiuyc5200 <17766728994@163.com>
2026-01-22 17:26:08 +0800

    Update dsi_display.c

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -1221,7 +1221,6 @@ int dsi_display_set_power(struct drm_connector *connector,
 {
 	struct dsi_display *display = disp;
 	struct msm_drm_notifier notify_data;
-	int event = power_mode;
 	int rc = 0;
 	struct drm_device *dev = NULL;
 
@@ -1239,7 +1238,8 @@ int dsi_display_set_power(struct drm_connector *connector,
             event = dev->doze_state;
 	}
 	
-	notify_data.data = &event;
+	notify_data.data = &power_mode;
+	notify_data.id = MSM_DRM_PRIMARY_DISPLAY;
 
 	switch (power_mode) {
 	case SDE_MODE_DPMS_LP1:
