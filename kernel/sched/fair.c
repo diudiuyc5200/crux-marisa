63608002990afdb4d9283f8189aa88ed6d1e7f17
diudiuyc5200 <17766728994@163.com>
2026-01-23 13:15:49 +0800

    Update fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12778,7 +12778,6 @@ void free_fair_sched_group(struct task_group *tg)
 {
 	int i;
 
-	destroy_cfs_bandwidth(tg_cfs_bandwidth(tg));
 
 	for_each_possible_cpu(i) {
 		if (tg->cfs_rq)
@@ -12856,6 +12855,8 @@ void unregister_fair_sched_group(struct task_group *tg)
 	struct rq *rq;
 	int cpu;
 
+	destroy_cfs_bandwidth(tg_cfs_bandwidth(tg));
+	
 	for_each_possible_cpu(cpu) {
 		if (tg->se[cpu])
 			remove_entity_load_avg(tg->se[cpu]);
