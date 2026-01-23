0cfa46245381d8c58a3ae4639995464672eaba44
diudiuyc5200 <17766728994@163.com>
2026-01-23 21:10:01 +0800

    Update inode.c

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -931,7 +931,12 @@ static void process_init_reply(struct fuse_conn *fc, struct fuse_req *req)
 				fc->default_permissions = 1;
 				fc->posix_acl = 1;
 				fc->sb->s_xattr = fuse_acl_xattr_handlers;
-			}
+				if (arg->flags & FUSE_PASSTHROUGH) {
+				fc->passthrough = 1;
+				/* Prevent further stacking */
+				fc->sb->s_stack_depth =
+					FILESYSTEM_MAX_STACK_DEPTH;
+				}
 		} else {
 			ra_pages = fc->max_read / PAGE_SIZE;
 			fc->no_lock = 1;
