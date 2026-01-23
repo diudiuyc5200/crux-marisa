3d315dc6a5e9f9a2423c4c69ab6dcec61fe1d017
diudiuyc5200 <17766728994@163.com>
2026-01-24 04:25:35 +0800

    Update dsi_panel.h

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -190,6 +190,12 @@ struct dsi_read_config {
 	u8 rbuf[BUF_LEN_MAX];
 };
 
+#define BRIGHTNESS_ALPHA_PAIR_LEN 2
+struct brightness_alpha_pair {
+	u32 brightness;
+	u32 alpha;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -300,6 +306,9 @@ struct dsi_panel {
 	bool fodflag;
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
+
+struct brightness_alpha_pair *fod_dim_lut;
+	u32 fod_dim_lut_count;
 
 	u8 panel_read_data[BUF_LEN_MAX];
 	struct dsi_read_config xy_coordinate_cmds;
@@ -427,6 +436,10 @@ struct dsi_panel *dsi_panel_ext_bridge_get(struct device *parent,
 int dsi_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
 
 void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
+
+int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
+
+u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 int dsi_panel_write_cmd_set(struct dsi_panel *panel, struct dsi_panel_cmd_set *cmd_sets);
 
