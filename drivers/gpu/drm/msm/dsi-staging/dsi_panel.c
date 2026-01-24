c5e3621751b71ce765e80cf0becc63a38b8e1991
diudiuyc5200 <17766728994@163.com>
2026-01-24 08:31:44 +0800

    Update dsi_panel.c

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -5658,6 +5658,8 @@ int dsi_panel_enable(struct dsi_panel *panel)
 
 	mutex_unlock(&panel->panel_lock);
 	pr_info("[SDE] %s: DSI_CMD_SET_ON\n", __func__);
+	if (panel->hbm_mode)
+		dsi_panel_apply_hbm_mode(panel);
 	return rc;
 }
 
