965cb77f80e7ba2653caf65c3f0f74f55bbc214c
diudiuyc5200 <17766728994@163.com>
2026-01-25 13:13:20 +0800

    Update core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2741,6 +2741,13 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 	return 0;
 }
 
+void sched_post_fork(struct task_struct *p)
+{
+#ifdef CONFIG_SCHED_BORE
+	sched_post_fork_bore(p);
+#endif // CONFIG_SCHED_BORE
+}
+
 unsigned long to_ratio(u64 period, u64 runtime)
 {
 	if (runtime == RUNTIME_INF)
