5b1a9d47cff965f02682c367ddfad978d6e7ecfe
diudiuyc5200 <17766728994@163.com>
2026-01-25 13:14:55 +0800

    Update core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6694,6 +6694,10 @@ void __init sched_init(void)
 	unsigned long alloc_size = 0, ptr;
 
 	sched_clock_init();
+	#ifdef CONFIG_SCHED_BORE
+	sched_init_bore();
+	printk(KERN_INFO "BORE (Burst-Oriented Response Enhancer) CPU Scheduler modification 5.1.0 by Masahito Suzuki");
+	#endif // CONFIG_SCHED_BORE
 	wait_bit_init();
 
 	init_clusters();
