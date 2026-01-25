fb4790c61c2ee77c0a828be5fd82850abf9b3c19
diudiuyc5200 <17766728994@163.com>
2026-01-26 03:39:41 +0800

    Update sde_crtc.c

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -2201,6 +2201,9 @@ static void _sde_crtc_blend_setup_mixer(struct drm_crtc *crtc,
 			_sde_crtc_setup_dim_layer_cfg(crtc, sde_crtc,
 					mixer, cstate->fingerprint_dim_layer);
 			SDE_ATRACE_END("dim layer blend setup mixer");
+			if (cstate->fod_dim_layer)
+			_sde_crtc_setup_dim_layer_cfg(crtc, sde_crtc,
+					mixer, cstate->fod_dim_layer);
 		}
 	}
 
