eb0fbbda0dc303e89280d9fa4a86291c51c42625
diudiuyc5200 <17766728994@163.com>
2026-01-27 02:49:02 +0800

    Update sysctl.c

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1165,6 +1165,17 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &two,
 	},
 #endif
+#ifdef CONFIG_USB
+	{
+		.procname	= "deny_new_usb",
+		.data		= &deny_new_usb,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax_sysadmin,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
+#endif
 	{
 		.procname	= "ngroups_max",
 		.data		= &ngroups_max,
