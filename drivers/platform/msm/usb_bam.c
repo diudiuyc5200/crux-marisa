930d43d8e2a702c87a9ecc1ff1ab7844d871403c
diudiuyc5200 <17766728994@163.com>
2026-01-26 12:15:57 +0800

    Update usb_bam.c

--- drivers/platform/msm/usb_bam.c
+++ drivers/platform/msm/usb_bam.c
@@ -1305,7 +1305,7 @@ retry:
 		}
 
 	       pipe_connect->enabled = false;
-	       spin_lock(&ctx->usb_bam_lock);
+		spin_lock(&ctx->usb_bam_lock);
 		if (ctx->pipes_enabled_per_bam == 0)
 			log_event_err("%s: wrong pipes enabled counter for bam=%d\n",
 				 __func__, pipe_connect->bam_type);
