0c6aec234e228e42675d7557a3a64863154970d4
diudiuyc5200 <17766728994@163.com>
2026-01-29 13:13:55 +0800

    Update wakeup.c

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -30,6 +30,16 @@ suspend_state_t pm_suspend_target_state;
 #define pm_suspend_target_state	(PM_SUSPEND_ON)
 #endif
 
+#ifdef CONFIG_BOEFFLA_WL_BLOCKER
+#include "boeffla_wl_blocker.h"
+
+char list_wl_search[LENGTH_LIST_WL_SEARCH] = {0};
+bool wl_blocker_active = false;
+bool wl_blocker_debug = false;
+
+static void wakeup_source_deactivate(struct wakeup_source *ws);
+#endif
+
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
  * if wakeup events are registered during or immediately before the transition.
