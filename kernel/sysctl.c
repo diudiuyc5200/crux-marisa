46e8f3801ae37d3e8984af01c6fabf1404242a61
diudiuyc5200 <17766728994@163.com>
2026-02-04 03:12:52 +0800

    Update sysctl.c

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -126,7 +126,9 @@ static int __maybe_unused neg_three = -3;
 static int zero;
 static int __maybe_unused one = 1;
 static int __maybe_unused two = 2;
+#ifndef CONFIG_SCHED_BORE
 static int __maybe_unused three = 3;
+#endif // CONFIG_SCHED_BORE
 static int __maybe_unused four = 4;
 static unsigned long zero_ul;
 static unsigned long one_ul = 1;
@@ -156,6 +158,20 @@ static int minolduid;
 
 static int ngroups_max = NGROUPS_MAX;
 static const int cap_last_cap = CAP_LAST_CAP;
+
+#ifdef CONFIG_SCHED_BORE
+extern uint sched_bore;
+extern uint sched_burst_score_rounding;
+extern uint sched_burst_smoothness_long;
+extern uint sched_burst_smoothness_short;
+extern uint sched_burst_fork_atavistic;
+extern uint sched_burst_penalty_offset;
+extern uint sched_burst_penalty_scale;
+extern uint sched_burst_cache_lifetime;
+static int __maybe_unused three          = 3;
+static int __maybe_unused sixty_four     = 64;
+static int __maybe_unused maxval_12_bits = 4095;
+#endif // CONFIG_SCHED_BORE
 
 /*this is needed for proc_doulongvec_minmax of sysctl_hung_task_timeout_secs */
 #ifdef CONFIG_DETECT_HUNG_TASK
