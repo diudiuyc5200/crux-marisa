ff8868f8656c2e55481cc2b395d11aa57e18168e
diudiuyc5200 <17766728994@163.com>
2026-02-04 02:59:35 +0800

    Update fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -798,6 +798,9 @@ static inline u64 calc_delta_fair(u64 delta, struct sched_entity *se)
 	if (unlikely(se->load.weight != NICE_0_LOAD))
 		delta = __calc_delta(delta, NICE_0_LOAD, &se->load);
 
+	#ifdef CONFIG_SCHED_BORE
+	if (likely(sched_bore)) delta = scale_slice(delta, se);
+#endif // CONFIG_SCHED_BORE
 	return delta;
 }
 
